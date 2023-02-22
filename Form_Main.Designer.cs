7f753f350a3de9da75f3b6f163230c2f2c156523
LingLu <71413848+Shemmejba@users.noreply.github.com>
2023-02-22 15:26:03 +0800

    Add files via upload

---
+++ Form_Main.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace AutoCloseComputer
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Main));
+            this.label_Time = new System.Windows.Forms.Label();
+            this.dateTimePicker_Time = new System.Windows.Forms.DateTimePicker();
+            this.button_StartOrEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Time
+            // 
+            this.label_Time.AutoSize = true;
+            this.label_Time.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_Time.Location = new System.Drawing.Point(18, 52);
+            this.label_Time.Name = "label_Time";
+            this.label_Time.Size = new System.Drawing.Size(48, 20);
+            this.label_Time.TabIndex = 0;
+            this.label_Time.Text = "时间 : ";
+            // 
+            // dateTimePicker_Time
+            // 
+            this.dateTimePicker_Time.CustomFormat = "";
+            this.dateTimePicker_Time.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker_Time.Location = new System.Drawing.Point(72, 50);
+            this.dateTimePicker_Time.Name = "dateTimePicker_Time";
+            this.dateTimePicker_Time.ShowUpDown = true;
+            this.dateTimePicker_Time.Size = new System.Drawing.Size(72, 23);
+            this.dateTimePicker_Time.TabIndex = 1;
+            // 
+            // button_StartOrEnd
+            // 
+            this.button_StartOrEnd.Location = new System.Drawing.Point(160, 48);
+            this.button_StartOrEnd.Name = "button_StartOrEnd";
+            this.button_StartOrEnd.Size = new System.Drawing.Size(75, 25);
+            this.button_StartOrEnd.TabIndex = 2;
+            this.button_StartOrEnd.Text = "定时关机";
+            this.button_StartOrEnd.UseVisualStyleBackColor = true;
+            this.button_StartOrEnd.Click += new System.EventHandler(this.button_StartOrEnd_Click);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 122);
+            this.Controls.Add(this.button_StartOrEnd);
+            this.Controls.Add(this.dateTimePicker_Time);
+            this.Controls.Add(this.label_Time);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "电脑控制程序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Time;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Time;
+        private System.Windows.Forms.Button button_StartOrEnd;
+    }
+}
+
---
+++ Form_Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCloseComputer
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 启动定时关机程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_StartOrEnd_Click(object sender, EventArgs e)
+        {
+            if (button_StartOrEnd.Text.Equals("定时关机"))
+            {
+                DateTime dateTime = dateTimePicker_Time.Value;
+                DateTime closeDateTime = DateTime.Now;
+                string str;
+                // 时间小于当前当前时间则认为是第二天的时间
+                if (dateTime.Hour < closeDateTime.Hour)
+                {
+                    str = closeDateTime.AddDays(1D).ToString("yyyy-MM-dd") + " " + dateTime.Hour.ToString().PadLeft(2, '0') + ":" + dateTime.Minute.ToString().PadLeft(2, '0');
+                    closeDateTime = Convert.ToDateTime(str);
+                }
+                else
+                {
+                    str = DateTime.Now.ToString("yyyy-MM-dd") + " " + dateTime.Hour.ToString().PadLeft(2, '0') + ":" + dateTime.Minute.ToString().PadLeft(2, '0');
+                    closeDateTime = Convert.ToDateTime(str);
+                }
+                // 计算出时间差
+                int seconds = Convert.ToInt32((closeDateTime - DateTime.Now).TotalSeconds);
+                OperateComputer("shutdown -s -t " + seconds);
+                button_StartOrEnd.Text = "取消定时";
+            }
+            else
+            {
+                OperateComputer("shutdown -a");
+                button_StartOrEnd.Text = "定时关机";
+            }
+        }
+
+        /// <summary>
+        /// 执行cmd命令
+        /// </summary>
+        /// <param name="command"></param>
+        private void OperateComputer(string command)
+        {
+            var startInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.CreateNoWindow = true;
+            var myProcess = new System.Diagnostics.Process();
+            myProcess.StartInfo = startInfo;
+            myProcess.Start();
+            myProcess.StandardInput.WriteLine(command);
+        }
+    }
+}
